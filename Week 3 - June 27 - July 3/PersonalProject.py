59eebbaca11da6f967d210f11f795318858c0f2e
Kayla <kwaddy@email.sc.edu>
2023-06-28 17:25:47 -0400

    Created a new folder for week 3 and created a personal project

---
+++ Week 3 - June 27 - July 3/PersonalProject.py
@@ -0,0 +1,9 @@
+# Title: Binary Computation
+# Written by Kayla Waddy
+# June 28th, 2023
+# Description: This personal project will explore how to write
+# a program in python to convert a binary input from the user
+# to a decimal number (i.e. from 10111000 to the decimal
+# equivalent). This wil explore the connection between two
+# different classes I am taking and how one's teachings
+# can be cross translated. Will update this as need be.
