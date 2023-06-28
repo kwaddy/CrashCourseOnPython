6f4fec5a7ac52d83658261f146ac073801e532ac
Kayla <kwaddy@email.sc.edu>
2023-06-28 17:50:12 -0400

    Created the beginning of my personal project to work on the baseline of how the program will work before checking into how to implement matricies

--- Week 3 - June 27 - July 3/PersonalProject.py
+++ Week 3 - June 27 - July 3/PersonalProject.py
@@ -7,3 +7,16 @@
 # equivalent). This wil explore the connection between two
 # different classes I am taking and how one's teachings
 # can be cross translated. Will update this as need be.
+
+# Defining how a function will read the user input
+# Specificially the input has to be done in ones and zeros
+def reading_input(x): 
+    str_binary = str(x) # Converts the input into a string
+    # Checks the length of the string and then determines weather
+    # a null number will print out or the length of the string.
+    # This was done to make sure that the program is working properly
+    # BEFORE further implementation
+    if (len(str_binary) < 8): 
+        return 0
+    return len(str_binary)
+print(reading_input())
