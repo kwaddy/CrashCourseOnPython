9a90c4ce98727ac5e312fe43a8c5e9cfa247ee67
Kayla <kwaddy@email.sc.edu>
2023-07-16 10:22:58 -0400

    Made a couple of new files for week 4

---
+++ Week 4/IteratingLists.py
@@ -0,0 +1,17 @@
+animals = ["Lion", "Zebra", "Dolphin", "Monkey"]
+chars = 0
+for animal in animals:
+    chars += len(animal)
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
+#Introducing the enumerating function
+winners = ["Ashley", "Dylan", "Reese"]
+for index, person in enumerate(winners):
+    print("{} - {}". format(index+1, person))
+
+# Making email addresses using enumerate by defining the function
+def full_emails(people):
+    result = []
+    for email, name in people:
+        result.append("{} <{}>".format(name,email))
+    return result
+print(full_emails([("alex@example.com", "Alex Diego"), ("shay@example.com", "Shay Brandt")]))
--- Week 4/StringPractice.py
+++ Week 4/StringPractice.py
@@ -2,3 +2,6 @@
 # Written by Kayla Waddy
 # This is a programming practice for strings and
 # string indexing.
+
+weather = "Rainfall"
+print(weather[4:])
